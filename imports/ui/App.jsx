98cf22011847c237129eadc4921c4217c18049a2
Aayush Kumar <Aayush>
2021-12-24 16:29:06 +0530

    Formatter

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,21 +1,16 @@
-import React from 'react';
-import { useTracker } from 'meteor/react-meteor-data';
-import './style.css';
-import { Landing } from './Landing';
-import { Feed } from './Feed.jsx';
-import { Route, Link} from 'react-router-dom';
+import React from "react";
+import { useTracker } from "meteor/react-meteor-data";
+import "./style.css";
+import { Landing } from "./Landing";
+import { Feed } from "./Feed.jsx";
+import { Route, Link } from "react-router-dom";
 export const App = () => {
-
   //const user = useTracker(() => Meteor.user());
   //localStorage.userId="";
-  return(
-      <div>
-        
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/feed" component={Feed} />
-
-
-        
-      </div>
+  return (
+    <div>
+      <Route exact path="/" component={Landing} />
+      <Route exact path="/feed" component={Feed} />
+    </div>
   );
-}
+};
